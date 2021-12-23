a14b1f94db7c51f4b3fd4387576fc950d4f82bd9
Isaiah Harrison <ijh5005@outlook.com>
2021-12-23 12:11:35 -0500

    pokemon refactor

--- js/pokemon.js
+++ js/pokemon.js
@@ -5,29 +5,15 @@ const getPokemonInfo = () => {
     fetch(pokemonUrl + text.toLowerCase())
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            const {
-                id,
-                name,
-                height,
-                weight,
-                sprites,
-                stats,
-                types
-            } = data;
-            const sprite = sprites.other.dream_world && sprites.other.dream_world.front_default || sprites.front_default;
-            const hp = stats[0].base_stat;
-            const atk = stats[1].base_stat;
-            const def = stats[2].base_stat;
-            document.getElementById("pokemon").style.backgroundImage = `url(${sprite})`
-            document.getElementById("pokemonId").innerText = id;
-            document.getElementById("pokemonName").innerText = name;
-            document.getElementById("pokemonHeight").innerText = height;
-            document.getElementById("pokemonWeight").innerText = weight;
-            document.getElementById("pokemonHp").innerText = hp;
-            document.getElementById("pokemonType").innerText = types[0].type.name;
-            document.getElementById("pokemonAtk").innerText = atk;
-            document.getElementById("pokemonDef").innerText = def;
+            document.getElementById("pokemon").style.backgroundImage = `url(${data.sprites.other.dream_world.front_default})`
+            document.getElementById("pokemonId").innerText = data.id;
+            document.getElementById("pokemonName").innerText = data.name;
+            document.getElementById("pokemonHeight").innerText = data.height;
+            document.getElementById("pokemonWeight").innerText = data.weight;
+            document.getElementById("pokemonHp").innerText = data.stats[0].base_stat;
+            document.getElementById("pokemonType").innerText = data.types[0].type.name;
+            document.getElementById("pokemonAtk").innerText = data.stats[1].base_stat;
+            document.getElementById("pokemonDef").innerText = data.stats[2].base_stat;
         })
         .catch(err => console.error(err));
 }
