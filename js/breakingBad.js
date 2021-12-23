11eb6455c8589fdb45ff019134233d3e30d62d10
Isaiah Harrison <ijh5005@outlook.com>
2021-12-23 12:00:22 -0500

    breaking bad refactor

--- js/breakingBad.js
+++ js/breakingBad.js
@@ -1,11 +1,19 @@
 const quoteUrl = "https://breaking-bad-quotes.herokuapp.com/v1/quotes";
 
+const setQuote = quote => {
+    document.getElementById("quoteText").innerText = quote;
+}
+
+const setAuthor = author => {
+    document.getElementById("breakingBadAuthor").innerText = author;
+}
+
 const getQuote = () => {
     fetch(quoteUrl)
         .then(response => response.json())
         .then(data => {
-            document.getElementById("quoteText").innerText = data[0].quote;
-            document.getElementById("breakingBadAuthor").innerText = data[0].author;
+            setQuote(data[0].quote);
+            setAuthor(data[0].author)
         })
         .catch(err => console.error(err));
 }
