720713a078aaa9b698481ddd9a14155d4d5f27e8
Isaiah Harrison <ijh5005@outlook.com>
2021-12-23 12:17:03 -0500

    tip calculator refactor

--- js/tipCalculator.js
+++ js/tipCalculator.js
@@ -1,14 +1,21 @@
+const allFieldsEntered = (cost, tip, numberOfPeople) => {
+    return cost.length && tip.length && numberOfPeople.length
+}
+
+const setTip = (cost, tip, numberOfPeople) => {
+    cost = parseFloat(cost);
+    tip = parseFloat(tip);
+    numberOfPeople = parseFloat(numberOfPeople);
+    const payPerPerson = (cost + cost * (tip / 100)) / numberOfPeople;
+    document.getElementById("pay").innerText = "$" + payPerPerson.toFixed(2);
+}
+
 const calculateTip = () => {
     let cost = document.getElementById("cost").value;
     let tip = document.getElementById("tip").value;
     let numberOfPeople = document.getElementById("numberOfPeople").value;
-    if(cost.length && tip.length && numberOfPeople.length){
-        cost = parseFloat(cost);
-        tip = parseFloat(tip);
-        numberOfPeople = parseFloat(numberOfPeople);
-
-        const payPerPerson = (cost + cost * (tip / 100)) / numberOfPeople;
-        document.getElementById("pay").innerText = "$" + payPerPerson.toFixed(2);
+    if(allFieldsEntered(cost, tip, numberOfPeople)){
+        setTip(cost, tip, numberOfPeople);
     }
 }
 
