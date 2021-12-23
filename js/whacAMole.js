2cfc699ed335133911cb9af9da3ba7f5525485d8
Isaiah Harrison <ijh5005@outlook.com>
2021-12-23 11:06:52 -0500

    text change

--- js/whacAMole.js
+++ js/whacAMole.js
@@ -11,7 +11,7 @@ let goHome = false;
 let canClick = true;
 
 const whakAMoleText = {
-    intro: "Wack the mole to score! Click to play.",
+    intro: "Whak the mole to score! Click to play.",
     endText: "You have scored X"
 }
 
