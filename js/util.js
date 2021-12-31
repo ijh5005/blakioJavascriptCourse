69592f4e0fb232b260f5a3c170698fb334ff4f00
Isaiah Harrison <ijh5005@outlook.com>
2021-12-30 20:51:53 -0500

    javascript complete

---
+++ js/util.js
@@ -0,0 +1,12 @@
+const offscreen = "offscreen";
+
+const toggleAttribute = (ele, attr, value="") => {
+    const hasAttribute = ele.hasAttribute(offscreen);
+    if(hasAttribute){
+        // remove the offset attribute
+        ele.removeAttribute(attr);
+    } else {
+        // add the offset attribute
+        ele.setAttribute(attr, value);
+    }
+}
